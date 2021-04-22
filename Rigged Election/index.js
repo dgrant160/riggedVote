6c32793cdfd145103aa291a57413aece4f103ff5
Dylan <dgrant60@west-mec.org>
2021-04-22 02:50:53 -0700

    got the follow troll to work

--- Rigged Election/index.js
+++ Rigged Election/index.js
@@ -4,7 +4,7 @@ let voteCount = {
     nixon: 60,
     mcgovern: 40,
     nixonVote() {
-        if(this.nixon >= 100){
+        if (this.nixon >= 100) {
             this.nixon = 100
             var audio = new Audio('US.mp3');
             audio.play();
@@ -12,17 +12,18 @@ let voteCount = {
             document.getElementById("flagBg").style.cssFloat = "initial"
             var myobj = document.getElementById("mcGovern");
             myobj.remove();
-        } else{
-        this.nixon++
-        this.mcgovern--
+        } else {
+            this.nixon++
+                this.mcgovern--
         }
     },
-    mcgovernVote(){
+    mcgovernVote() {
         this.nixon = 100;
         this.mcgovern = 0;
-        
-        document.getElementById("mcGovern").addEventListener("mousemove", followtroll);
-        function followtroll(){
+
+        document.getElementById("body").addEventListener("mousemove", followtroll);
+
+        function followtroll() {
             var x = event.clientX;
             var y = event.clientY;
             var node = document.getElementById("followtroll")
@@ -35,46 +36,52 @@ let voteCount = {
         document.getElementById("nixonBar").style.width = this.nixon + "%"
         document.getElementById("nixonBar").innerHTML = this.nixon + "% Nixon";
         document.getElementById("mcgovernBar").style.width = this.mcgovern + "%"
-        document.getElementById("mcgovernBar").innerHTML = this.mcgovern +"% McGovern";
+        document.getElementById("mcgovernBar").innerHTML = this.mcgovern + "% McGovern";
     },
 }
-function troll(){
-    if(trollUp != false){
+
+function troll() {
+    if (trollUp != false) {
         window.location.replace("https://www.youtube.com/watch?v=QtBDL8EiNZo");
-    }else{
-    voteCount.mcgovernVote()
-    voteCount.updateVote()
-    document.getElementById("troll").style.opacity += 1
-    document.getElementById("troll").style.cssFloat = "initial"
-    document.getElementById('trollFrame').src='dancing_troll.gif'}
+    } else {
+        voteCount.mcgovernVote()
+        voteCount.updateVote()
+        document.getElementById("troll").style.opacity += 1
+        document.getElementById("troll").style.cssFloat = "initial"
+        document.getElementById('trollFrame').src = 'dancing_troll.gif'
+    }
     trollUp = true
     var audio = new Audio('US.mp3');
-        audio.play();
+    audio.play();
 }
-function vote(){
+
+function vote() {
     alert("You should vote for Nixon unless you want something terrible to happen this is a promise not a threat")
 }
-function dancingTroll(){
-    document.getElementById('trollFrame').src='dancing_troll.gif'
-}
-function normal(){
-    if (trollUp != false) {
-        document.getElementById('trollFrame').src='dancing_troll.gif'
-    } else{
-    document.getElementById('trollFrame').src='mcgovern.png'}
-}
-function correctVote(){
-    voteCount.nixonVote()
-    voteCount.updateVote() 
-}
-function addClass(){
-    if(run1 != false){
 
-    }else{
+function dancingTroll() {
+    document.getElementById('trollFrame').src = 'dancing_troll.gif'
+}
+
+function normal() {
+    if (trollUp != false) {
+        document.getElementById('trollFrame').src = 'dancing_troll.gif'
+    } else {
+        document.getElementById('trollFrame').src = 'mcgovern.png'
+    }
+}
+
+function correctVote() {
+    voteCount.nixonVote()
+    voteCount.updateVote()
+}
+
+function addClass() {
+    if (run1 != false) {
+
+    } else {
         run1 = true;
         document.getElementById('body').classList.add("addClass")
     }
 
 }
-
-
